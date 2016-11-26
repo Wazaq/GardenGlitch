1ebce5298092726c2992c3ecd847013eb004319e
Wazaq <Brent.Watkins32@gmail.com>
2016-11-26 00:25:34 -0600

    Changes to Alpha 0.2
    Changes to Alpha 0.2

---
+++ Assets/Scripts/FastForward.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FastForward : MonoBehaviour {
+
+	public bool speedUp = false;
+
+	void SpeedUpTheGame() {
+		if (speedUp) {
+			Time.timeScale = 2;
+		} else {
+			Time.timeScale = 1;
+		}
+	}
+
+}
---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+	public bool isPaused = false;
+
+	private GameObject pauseScreen;
+
+
+
+	void Start () {
+		pauseScreen = GameObject.FindGameObjectWithTag("PauseScreen");
+		pauseScreen.SetActive(false);
+
+	}
+
+	public void PauseScreen(bool isPaused) {
+		if (isPaused) {
+			pauseScreen.SetActive(true);
+			Time.timeScale = 0;
+		} else {
+			pauseScreen.SetActive(false);
+			Time.timeScale = 1;
+		}
+		
+	}
+
+}
